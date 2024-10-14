d86b8e624777fd2b68192533a0f4f32d2de19df5
FBetulDemir <fatmabetuld@gmail.com>
2024-10-14 07:19:27 +0200

    Added interface WeatherProps

--- src/components/Weather.tsx
+++ src/components/Weather.tsx
@@ -1,6 +1,22 @@
-const Weather = () => {
+import React, {useState, useEffect} from 'react';
+import SearchBar from './SearchBar';
+
+  
+interface WeatherProps {
+    lat: number;
+    lon: number;
+}
+
+const Weather: WeatherProps = ({lat, lng}) => {
+    const (weatherData, setWeatherData) = useState <any | null>;
+    const [isLoading, setIsLoading] = useState(false);
+    const [error, setError] = useState<string | null>(null);
+
+
     return ( 
- 
+        <div className="weather-container">
+            
+        </div>
     );
 }
  
