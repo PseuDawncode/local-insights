c76ae396337e6b2601fb98d9875a95abf9d71291
mdmalida <dawnmarymalida@gmail.com>
2024-10-24 17:39:47 +0200

    Update UserInpuut folder

--- src/App.tsx
+++ src/App.tsx
@@ -4,17 +4,14 @@ import Departures from './components/Departures';
 
 
 const App: React.FC = () => {
-  const handleSearch = (searchTerm: string) => {
-    console.log('Search term:', searchTerm);
-    
-  };
+
 
   return (
     <div className="App">
       <Header 
-        logoUrl="https://example.com/logo.png" 
+        logoSrc="/images/logo.png"
         title="Local Travel and Weather Dashboard"
-        onSearch={handleSearch} 
+        
       />
       {/* logic */}
       <div className='main-container'>
